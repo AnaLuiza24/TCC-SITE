1246b6c728ec28bc0f5361b60ea4d58d73ff84f5
Kauan <mvkauan4@gmail.com>
2023-10-18 16:33:45 -0300

    w

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import ListaCliente from './pages/adm/listaCliente';
 import ListaProduto from './pages/adm/listaProduto';
 import AdicionarProduto from './pages/adm/adicionarProduto';
 import LandingPage from './pages/client/ladingPage';
+import Pagamento from './pages/client/pagamento';
 import {register} from 'swiper/element/bundle'
+import Nendereco from './pages/client/Endereço';
 
 register();
 import 'swiper/css';
@@ -37,6 +39,9 @@ root.render(
         <Route path='/listaCliente' element={<ListaCliente />} />
         <Route path='/listaProduto' element={<ListaProduto />} />
         <Route path='/adicionarProduto' element={<AdicionarProduto />} />
+        <Route path='/pagamento' element={<Pagamento />} />
+        <Route path='/editendereco' element={<Nendereco />} />
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
