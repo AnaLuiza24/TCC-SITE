95a4299cd5a8fabf433f2f4c9e557939972f2b2a
jorgestark <jorgestark278@gmail.com>
2023-10-20 14:07:11 -0300

    ok

---
+++ src/pages/confirmação-de-entrega/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import Cabecalho from '../../components/cabecalho'
+
+
+export default function Confirmação(){
+    return(
+        <div>
+            <Cabecalho/>
+
+            <div className=''>
+
+            </div>
+
+            
+        </div>
+    )
+}
