97390ffc49d97b2ff2f4054c62ce9e244563a312
jorgebiz19 <jorgestark278@gmail.com>
2023-11-23 22:54:02 -0300

    arrumando pagina home

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ root.render(
     <BrowserRouter>
     <ToastContainer />
       <Routes>
-        <Route path='/home' element={<LandingPage />} />
+        <Route path='/' element={<LandingPage />} />
         <Route path='/sobre-nos' element={<SobreNos />} />
         <Route path='/politica-privacidade' element={<PrivacyPolicy />} />
         <Route path='/login' element={<Login />} />
